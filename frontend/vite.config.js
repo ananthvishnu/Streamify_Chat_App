9ccd7d64ad477ce732d93051ba8259ee03e96124
Ananthakumar Vishnurathan <124557188+ananthvishnu@users.noreply.github.com>
2025-08-30 20:52:32 +0530

    initial commit 2

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,6 +5,6 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: "build", // Ensure this matches your expected directory
+    outDir: "dist", // Ensure this matches your expected directory
   },
 });
